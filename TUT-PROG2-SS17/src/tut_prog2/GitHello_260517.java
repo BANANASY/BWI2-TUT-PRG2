52debe1d992a40643b34c0eeebb2287c0a7b063c
Pinkaro <schaller.alexander@hotmail.com>
2017-05-26 16:52:29 +0200

    push GitHello

---
+++ TUT-PROG2-SS17/src/tut_prog2/GitHello_260517.java
@@ -0,0 +1,13 @@
+package tut_prog2;
+
+import util.TextIO;
+
+public class GitHello_260517 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TextIO.putln("Hello TUT!");
+		
+	}
+
+}
